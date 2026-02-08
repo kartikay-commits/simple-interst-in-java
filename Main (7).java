1d067e25902bb671e59f2d3359d1fa1e63fc335e
kartikay singh <kartikaysin566@gmail.com>
2026-02-08 10:21:18 +0530

    Add files via upload

---
+++ Main (7).java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+	float P=1 , R =1 , T=1;
+	float SI = (P*R*T)/100;
+	System.out.print("Simple interest = " + SI);
+	
+	}
+}
